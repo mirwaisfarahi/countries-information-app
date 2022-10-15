1045b1441127cea7cc6c1481208bc40cd702221d
Mirwais Farahi <waisfarahi@gmail.com>
2022-10-15 14:20:24 +0500

    Edit App.js File

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/configureStore';
+import HomePage from './components/HomePage';
+import Header from './components/Header';
 import './App.css';
+import './index.css';
+import DetailsPage from './components/DetailsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
 
 export default App;
