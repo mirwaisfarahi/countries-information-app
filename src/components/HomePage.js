083de8c879d27218018ec6d7214019a216e46dbc
Mirwais Farahi <waisfarahi@gmail.com>
2022-10-16 01:31:26 +0500

    Edit HomePage.js File

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { fetchCountries } from '../redux/country/country';
 import { fetchCountrySpotlight } from '../redux/countrySpotlight/countrySpotlight';
 import Country from './Country';
 import '../App.css';
@@ -7,11 +8,45 @@ import '../index.css';
 
 const HomePage = () => {
   const { countrySpotlight } = useSelector((state) => state.countrySpotlight);
+  const { countriesList } = useSelector((state) => state.countries);
+  const countriesInputRef = useRef();
   const dispatch = useDispatch();
+
+  const [filter, setFilter] = useState(countriesList);
+
+  const filterCountries = (e) => {
+    const target = e.target.value;
+    if (target === 'all') {
+      setFilter(countriesList);
+    } else {
+      setFilter(countriesList.filter((el) => el.region === target));
+    }
+  };
+
+  const filterCountriesByName = () => {
+    const searchValue = countriesInputRef.current.value;
+
+    if (searchValue.trim()) {
+      const fetchSearch = async () => {
+        const req = await fetch(`https://restcountries.com/v3.1/name/${searchValue}`);
+        const res = await req.json();
+        setFilter(res);
+      };
+      try {
+        fetchSearch();
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setFilter(countriesList);
+    }
+  };
 
   useEffect(() => {
     dispatch(fetchCountrySpotlight());
-  }, []);
+    dispatch(fetchCountries());
+    setFilter(null);
+  }, [dispatch]);
 
   return (
     <div className="countries-container">
@@ -34,9 +69,21 @@ const HomePage = () => {
         </div>
       ))}
       <div className="population-stats">
-        <h2 className="stats-title">STATS BY COUNTRY POPULATION</h2>
+        <input className="stats-search" type="text" ref={countriesInputRef} onChange={filterCountriesByName} />
+        <select
+          className="stats-select"
+          name="drop-down"
+          onChange={filterCountries}
+        >
+          <option value="all">All</option>
+          <option value="Asia">Asia</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">America</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
       </div>
-      <Country />
+      <Country countries={filter || countriesList} />
     </div>
   );
 };
