614e8d7d691a3705cbe6b943cbe604e197f2ec75
Mirwais Farahi <waisfarahi@gmail.com>
2022-10-15 16:18:10 +0500

    Edit Country.js File

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
